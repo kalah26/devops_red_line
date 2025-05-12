9886c4301b85d4d093eb3a6c237f20c121282f1a
Moussa Aminata Gueye <kalamouu@gmail.com>
2025-05-12 13:01:50 +0000

    Updated the dockerfiles to use kubernetes

--- Frontend/src/api.jsx
+++ Frontend/src/api.jsx
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: "http://192.168.39.48:30519/api",
 });
 
 export const UseAxios = () => {
--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', 
+  baseURL: 'http://192.168.39.48:30519/api', 
 });
 
 
